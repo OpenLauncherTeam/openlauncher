262736dcac764bd6d3078c0a019f705ff0d0d7ae
BennyKok <itechbenny@gmail.com>
2016-12-19 21:19:27 +0800

    bugs fixed : crash on stop

--- launcher/src/main/java/com/benny/openlauncher/util/LauncherSettings.java
+++ launcher/src/main/java/com/benny/openlauncher/util/LauncherSettings.java
@@ -176,6 +176,7 @@ public class LauncherSettings {
     }
 
     public Gson writeSettings() {
+        if (generalSettings == null)return null;
         generalSettings.firstLauncher = false;
 
         Gson gson = new Gson();
