4adda73dbd0782d2303c40b9ac9e4aa48d5602a4
Gaukler Faun <baumann.juergen@posteo.de>
2017-01-23 17:07:49 +0100

    more string resources, hide app icon

---
+++ launcher/src/main/java/com/benny/openlauncher/Activity_start.java
@@ -0,0 +1,22 @@
+package com.benny.openlauncher;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.benny.openlauncher.activity.Home;
+
+
+public class Activity_start extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(Activity_start.this, Home.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+
+        finish();
+    }
+}
