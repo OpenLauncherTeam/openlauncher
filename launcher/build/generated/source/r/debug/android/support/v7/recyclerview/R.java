e54627acf69b13251af619e5a21fdab62253e34e
Gaukler Faun <baumann.juergen@posteo.de>
2017-01-24 18:34:33 +0100

    added intro screen ;-)

--- launcher/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ launcher/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -14,9 +14,9 @@ public final class R {
 		public static final int stackFromEnd = 0x7f010172;
 	}
 	public static final class dimen {
-		public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f090086;
-		public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f090087;
-		public static final int item_touch_helper_swipe_escape_velocity = 0x7f090088;
+		public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f090089;
+		public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f09008a;
+		public static final int item_touch_helper_swipe_escape_velocity = 0x7f09008b;
 	}
 	public static final class id {
 		public static final int item_touch_helper_previous_elevation = 0x7f0e0007;
