55562afa710be32eaed0b425faa7b6081c5b42d5
Gregor Santner <gsantner@mailbox.org>
2017-05-29 19:08:50 +0200

    Update opoc/AppSettingsBase

--- app/src/main/java/io/github/gsantner/opoc/util/AppSettingsBase.java
+++ app/src/main/java/io/github/gsantner/opoc/util/AppSettingsBase.java
@@ -1,10 +1,12 @@
 /*
- * ----------------------------------------------------------------------------
- * "THE COKE-WARE LIBRARY LICENSE" (Revision 255):
+ * ---------------------------------------------------------------------------- *
  * Gregor Santner <gsantner.github.io> wrote this file. You can do whatever
  * you want with this stuff. If we meet some day, and you think this stuff is
  * worth it, you can buy me a coke in return. Provided as is without any kind
  * of warranty. No attribution required.                  - Gregor Santner
+ *
+ * License: Creative Commons Zero (CC0 1.0)
+ *  http://creativecommons.org/publicdomain/zero/1.0/
  * ----------------------------------------------------------------------------
  */
 
