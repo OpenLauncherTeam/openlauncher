6acdc65a21e6383846ad2f1c7f711dde7d8af52f
Anton Serebryakov <serebryakov.anton@gmail.com>
2018-10-20 20:13:15 +0300

    Painting MoreInfoActivity's toolbar with primaryColor. (#431)

--- app/src/main/java/com/benny/openlauncher/activity/MoreInfoActivity.java
+++ app/src/main/java/com/benny/openlauncher/activity/MoreInfoActivity.java
@@ -12,8 +12,10 @@ public class MoreInfoActivity extends ThemeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setBackgroundColor(_appSettings.getPrimaryColor());
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
