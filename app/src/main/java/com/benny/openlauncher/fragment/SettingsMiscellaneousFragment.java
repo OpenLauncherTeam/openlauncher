84153bfa976aedfaebdd9ba57b69399e246ebb6b
dkanada <dkanada@users.noreply.github.com>
2018-10-26 03:34:31 +0900

    kind of implement option to turn off screen

--- app/src/main/java/com/benny/openlauncher/fragment/SettingsMiscellaneousFragment.java
+++ app/src/main/java/com/benny/openlauncher/fragment/SettingsMiscellaneousFragment.java
@@ -33,6 +33,22 @@ public class SettingsMiscellaneousFragment extends SettingsBaseFragment {
         HomeActivity homeActivity = HomeActivity._launcher;
         int key = new ContextUtils(homeActivity).getResId(ContextUtils.ResType.STRING, preference.getKey());
         switch (key) {
+            case R.string.pref_key__backup:
+                if (new PermissionChecker(getActivity()).doIfExtStoragePermissionGranted()) {
+                    Intent i = new Intent(getActivity(), FilePickerActivity.class)
+                            .putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, true)
+                            .putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_DIR);
+                    getActivity().startActivityForResult(i, Definitions.INTENT_BACKUP);
+                }
+                return true;
+            case R.string.pref_key__restore:
+                if (new PermissionChecker(getActivity()).doIfExtStoragePermissionGranted()) {
+                    Intent i = new Intent(getActivity(), FilePickerActivity.class)
+                            .putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false)
+                            .putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
+                    getActivity().startActivityForResult(i, Definitions.INTENT_RESTORE);
+                }
+                return true;
             case R.string.pref_key__reset_settings:
                 DialogHelper.alertDialog(getActivity(), getString(R.string.pref_title__reset_settings), getString(R.string.are_you_sure), new MaterialDialog.SingleButtonCallback() {
                     @Override
@@ -58,22 +74,6 @@ public class SettingsMiscellaneousFragment extends SettingsBaseFragment {
                         System.exit(0);
                     }
                 });
-                return true;
-            case R.string.pref_key__backup:
-                if (new PermissionChecker(getActivity()).doIfExtStoragePermissionGranted()) {
-                    Intent i = new Intent(getActivity(), FilePickerActivity.class)
-                            .putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, true)
-                            .putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_DIR);
-                    getActivity().startActivityForResult(i, Definitions.INTENT_BACKUP);
-                }
-                return true;
-            case R.string.pref_key__restore:
-                if (new PermissionChecker(getActivity()).doIfExtStoragePermissionGranted()) {
-                    Intent i = new Intent(getActivity(), FilePickerActivity.class)
-                            .putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false)
-                            .putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
-                    getActivity().startActivityForResult(i, Definitions.INTENT_RESTORE);
-                }
                 return true;
             case R.string.pref_key__restart:
                 homeActivity.recreate();
