47a9ff75e2f630a8991c308cb55f574bb66e978c
dkanada <dkanada@users.noreply.github.com>
2018-12-24 07:49:34 +0900

    move widget logic to separate method

--- app/src/main/java/com/benny/openlauncher/viewutil/ItemViewFactory.java
+++ app/src/main/java/com/benny/openlauncher/viewutil/ItemViewFactory.java
@@ -25,143 +25,149 @@ import com.benny.openlauncher.widget.WidgetView;
 
 public class ItemViewFactory {
     public static View getItemView(final Context context, final DesktopCallback callback, final DragAction.Action type, final Item item) {
-        AppItemView.Builder builder = new AppItemView.Builder(context);
-        builder.setIconSize(Setup.appSettings().getIconSize());
-        builder.vibrateWhenLongPress(Setup.appSettings().getGestureFeedback());
-        builder.withOnLongClick(item, type, callback);
         View view = null;
-
-        switch(type) {
-            case DRAWER:
-                builder.setLabelVisibility(Setup.appSettings().getDrawerShowLabel());
-                builder.setTextColor(Setup.appSettings().getDrawerLabelColor());
-                break;
-            case DESKTOP:
-            default:
-                builder.setLabelVisibility(Setup.appSettings().getDesktopShowLabel());
-                builder.setTextColor(Color.WHITE);
-                break;
+        if (item.getType().equals(Item.Type.WIDGET)) {
+            view = getWidgetView(context, callback, type, item);
+        } else {
+            AppItemView.Builder builder = new AppItemView.Builder(context);
+            builder.setIconSize(Setup.appSettings().getIconSize());
+            builder.vibrateWhenLongPress(Setup.appSettings().getGestureFeedback());
+            builder.withOnLongClick(item, type, callback);
+            switch(type) {
+                case DRAWER:
+                    builder.setLabelVisibility(Setup.appSettings().getDrawerShowLabel());
+                    builder.setTextColor(Setup.appSettings().getDrawerLabelColor());
+                    break;
+                case DESKTOP:
+                default:
+                    builder.setLabelVisibility(Setup.appSettings().getDesktopShowLabel());
+                    builder.setTextColor(Color.WHITE);
+                    break;
+            }
+            switch (item.getType()) {
+                case APP:
+                    final App app = Setup.appLoader().findItemApp(item);
+                    if (app == null) break;
+                    view = builder.setAppItem(item).getView();
+                    break;
+                case SHORTCUT:
+                    view = builder.setShortcutItem(item).getView();
+                    break;
+                case GROUP:
+                    view = builder.setGroupItem(context, callback, item).getView();
+                    view.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+                    break;
+                case ACTION:
+                    view = builder.setActionItem(item).getView();
+                    break;
+            }
         }
-        switch (item.getType()) {
-            case APP:
-                final App app = Setup.appLoader().findItemApp(item);
-                if (app == null) break;
-                view = builder.setAppItem(item).getView();
-                break;
-            case SHORTCUT:
-                view = builder.setShortcutItem(item).getView();
-                break;
-            case GROUP:
-                view = builder.setGroupItem(context, callback, item).getView();
-                view.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-                break;
-            case ACTION:
-                view = builder.setActionItem(item).getView();
-                break;
-            case WIDGET:
-                if (HomeActivity._appWidgetHost == null) break;
-                final AppWidgetProviderInfo appWidgetInfo = HomeActivity._appWidgetManager.getAppWidgetInfo(item.getWidgetValue());
-                final WidgetView widgetView = (WidgetView) HomeActivity._appWidgetHost.createView(context, item.getWidgetValue(), appWidgetInfo);
 
-                widgetView.setAppWidget(item.getWidgetValue(), appWidgetInfo);
-                widgetView.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        updateWidgetOption(item);
-                    }
-                });
-
-                final FrameLayout widgetContainer = (FrameLayout) LayoutInflater.from(context).inflate(R.layout.view_widget_container, null);
-                widgetContainer.addView(widgetView);
-
-                final View ve = widgetContainer.findViewById(R.id.vertexpand);
-                ve.bringToFront();
-                final View he = widgetContainer.findViewById(R.id.horiexpand);
-                he.bringToFront();
-                final View vl = widgetContainer.findViewById(R.id.vertless);
-                vl.bringToFront();
-                final View hl = widgetContainer.findViewById(R.id.horiless);
-                hl.bringToFront();
-
-                ve.animate().scaleY(1).scaleX(1);
-                he.animate().scaleY(1).scaleX(1);
-                vl.animate().scaleY(1).scaleX(1);
-                hl.animate().scaleY(1).scaleX(1);
-
-                final Runnable action = new Runnable() {
-                    @Override
-                    public void run() {
-                        ve.animate().scaleY(0).scaleX(0);
-                        he.animate().scaleY(0).scaleX(0);
-                        vl.animate().scaleY(0).scaleX(0);
-                        hl.animate().scaleY(0).scaleX(0);
-                    }
-                };
-
-                widgetContainer.postDelayed(action, 2000);
-                // TODO move this to standard DragHandler.getLongClick() method
-                // needs to be set on widgetView but use widgetContainer inside
-                widgetView.setOnLongClickListener(new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View view) {
-                        if (Setup.appSettings().getDesktopLock()) {
-                            return false;
-                        }
-                        if (Setup.appSettings().getGestureFeedback()) {
-                            Tool.vibrate(view);
-                        }
-                        DragHandler.startDrag(widgetContainer, item, DragAction.Action.DESKTOP, callback);
-                        return true;
-                    }
-                });
-
-                ve.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if (view.getScaleX() < 1) return;
-                        item.setSpanY(item.getSpanY() + 1);
-                        scaleWidget(widgetContainer, item);
-                        widgetContainer.removeCallbacks(action);
-                        widgetContainer.postDelayed(action, 2000);
-                    }
-                });
-                he.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if (view.getScaleX() < 1) return;
-                        item.setSpanX(item.getSpanX() + 1);
-                        scaleWidget(widgetContainer, item);
-                        widgetContainer.removeCallbacks(action);
-                        widgetContainer.postDelayed(action, 2000);
-                    }
-                });
-                vl.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if (view.getScaleX() < 1) return;
-                        item.setSpanY(item.getSpanY() - 1);
-                        scaleWidget(widgetContainer, item);
-                        widgetContainer.removeCallbacks(action);
-                        widgetContainer.postDelayed(action, 2000);
-                    }
-                });
-                hl.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if (view.getScaleX() < 1) return;
-                        item.setSpanX(item.getSpanX() - 1);
-                        scaleWidget(widgetContainer, item);
-                        widgetContainer.removeCallbacks(action);
-                        widgetContainer.postDelayed(action, 2000);
-                    }
-                });
-                view = widgetContainer;
-                break;
-        }
+        // TODO find out why tag is set here
         if (view != null) {
             view.setTag(item);
         }
         return view;
+    }
+
+    public static View getWidgetView(final Context context, final DesktopCallback callback, final DragAction.Action type, final Item item) {
+        if (HomeActivity._appWidgetHost == null) return null;
+        final AppWidgetProviderInfo appWidgetInfo = HomeActivity._appWidgetManager.getAppWidgetInfo(item.getWidgetValue());
+        final WidgetView widgetView = (WidgetView) HomeActivity._appWidgetHost.createView(context, item.getWidgetValue(), appWidgetInfo);
+
+        widgetView.setAppWidget(item.getWidgetValue(), appWidgetInfo);
+        widgetView.post(new Runnable() {
+            @Override
+            public void run() {
+                updateWidgetOption(item);
+            }
+        });
+
+        final FrameLayout widgetContainer = (FrameLayout) LayoutInflater.from(context).inflate(R.layout.view_widget_container, null);
+        widgetContainer.addView(widgetView);
+
+        final View ve = widgetContainer.findViewById(R.id.vertexpand);
+        ve.bringToFront();
+        final View he = widgetContainer.findViewById(R.id.horiexpand);
+        he.bringToFront();
+        final View vl = widgetContainer.findViewById(R.id.vertless);
+        vl.bringToFront();
+        final View hl = widgetContainer.findViewById(R.id.horiless);
+        hl.bringToFront();
+
+        ve.animate().scaleY(1).scaleX(1);
+        he.animate().scaleY(1).scaleX(1);
+        vl.animate().scaleY(1).scaleX(1);
+        hl.animate().scaleY(1).scaleX(1);
+
+        final Runnable action = new Runnable() {
+            @Override
+            public void run() {
+                ve.animate().scaleY(0).scaleX(0);
+                he.animate().scaleY(0).scaleX(0);
+                vl.animate().scaleY(0).scaleX(0);
+                hl.animate().scaleY(0).scaleX(0);
+            }
+        };
+
+        widgetContainer.postDelayed(action, 2000);
+        // TODO move this to standard DragHandler.getLongClick() method
+        // needs to be set on widgetView but use widgetContainer inside
+        widgetView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                if (Setup.appSettings().getDesktopLock()) {
+                    return false;
+                }
+                if (Setup.appSettings().getGestureFeedback()) {
+                    Tool.vibrate(view);
+                }
+                DragHandler.startDrag(widgetContainer, item, DragAction.Action.DESKTOP, callback);
+                return true;
+            }
+        });
+
+        ve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getScaleX() < 1) return;
+                item.setSpanY(item.getSpanY() + 1);
+                scaleWidget(widgetContainer, item);
+                widgetContainer.removeCallbacks(action);
+                widgetContainer.postDelayed(action, 2000);
+            }
+        });
+        he.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getScaleX() < 1) return;
+                item.setSpanX(item.getSpanX() + 1);
+                scaleWidget(widgetContainer, item);
+                widgetContainer.removeCallbacks(action);
+                widgetContainer.postDelayed(action, 2000);
+            }
+        });
+        vl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getScaleX() < 1) return;
+                item.setSpanY(item.getSpanY() - 1);
+                scaleWidget(widgetContainer, item);
+                widgetContainer.removeCallbacks(action);
+                widgetContainer.postDelayed(action, 2000);
+            }
+        });
+        hl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getScaleX() < 1) return;
+                item.setSpanX(item.getSpanX() - 1);
+                scaleWidget(widgetContainer, item);
+                widgetContainer.removeCallbacks(action);
+                widgetContainer.postDelayed(action, 2000);
+            }
+        });
+        return widgetContainer;
     }
 
     private static void scaleWidget(View view, Item item) {
