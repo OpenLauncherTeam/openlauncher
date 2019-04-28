65321b5c87e78802123d70f1b1c166f48159a322
Gregor Santner <gsantner@mailbox.org>
2019-04-28 19:22:27 +0200

    Migrate Settings: Miscellaneous

--- app/src/main/java/com/benny/openlauncher/fragment/SettingsMasterFragment.java
+++ app/src/main/java/com/benny/openlauncher/fragment/SettingsMasterFragment.java
@@ -3,20 +3,31 @@ package com.benny.openlauncher.fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 import android.text.TextUtils;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.benny.openlauncher.R;
+import com.benny.openlauncher.activity.HomeActivity;
 import com.benny.openlauncher.activity.MoreInfoActivity;
 import com.benny.openlauncher.activity.SettingsActivity;
 import com.benny.openlauncher.util.AppSettings;
+import com.benny.openlauncher.util.DatabaseHelper;
+import com.benny.openlauncher.util.Definitions;
+import com.benny.openlauncher.viewutil.DialogHelper;
 import com.benny.openlauncher.widget.AppDrawerController;
+import com.nononsenseapps.filepicker.FilePickerActivity;
 
 import net.gsantner.opoc.preference.GsPreferenceFragmentCompat;
+import net.gsantner.opoc.util.PermissionChecker;
 
+import java.io.File;
 import java.util.Locale;
 
 public class SettingsMasterFragment extends GsPreferenceFragmentCompat<AppSettings> {
@@ -84,9 +95,61 @@ public class SettingsMasterFragment extends GsPreferenceFragmentCompat<AppSettin
     @Override
     @SuppressWarnings({"ConstantIfStatement"})
     public Boolean onPreferenceClicked(Preference preference, String key, int keyResId) {
+        HomeActivity homeActivity = HomeActivity._launcher;
         switch (keyResId) {
             case R.string.pref_key__about: {
                 startActivity(new Intent(getActivity(), MoreInfoActivity.class));
+                return true;
+            }
+            case R.string.pref_key__backup: {
+                if (new PermissionChecker(getActivity()).doIfExtStoragePermissionGranted()) {
+                    Intent i = new Intent(getActivity(), FilePickerActivity.class)
+                            .putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, true)
+                            .putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_DIR);
+                    getActivity().startActivityForResult(i, Definitions.INTENT_BACKUP);
+                }
+                return true;
+            }
+            case R.string.pref_key__restore: {
+                if (new PermissionChecker(getActivity()).doIfExtStoragePermissionGranted()) {
+                    Intent i = new Intent(getActivity(), FilePickerActivity.class)
+                            .putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false)
+                            .putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
+                    getActivity().startActivityForResult(i, Definitions.INTENT_RESTORE);
+                }
+                return true;
+            }
+            case R.string.pref_key__reset_settings: {
+                DialogHelper.alertDialog(getActivity(), getString(R.string.pref_title__reset_settings), getString(R.string.are_you_sure), new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        try {
+                            PackageInfo p = getActivity().getPackageManager().getPackageInfo(getContext().getPackageName(), 0);
+                            String dataDir = p.applicationInfo.dataDir;
+                            new File(dataDir + "/shared_prefs/app.xml").delete();
+                            System.exit(0);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                return true;
+            }
+            case R.string.pref_key__reset_database: {
+                DialogHelper.alertDialog(getActivity(), getString(R.string.pref_title__reset_database), getString(R.string.are_you_sure), new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        DatabaseHelper db = HomeActivity._db;
+                        db.onUpgrade(db.getWritableDatabase(), 1, 1);
+                        AppSettings.get().setAppFirstLaunch(true);
+                        System.exit(0);
+                    }
+                });
+                return true;
+            }
+            case R.string.pref_key__restart: {
+                homeActivity.recreate();
+                getActivity().finish();
                 return true;
             }
         }
