f37ef18ba39d056e69e72fbd7bf2cbc1ede08592
Vincent Wilms <vwilms@outlook.com>
2021-01-03 22:20:40 +0100

    Improve shortcut feautre.

--- app/src/main/java/com/benny/openlauncher/activity/AddShortcutActivity.java
+++ app/src/main/java/com/benny/openlauncher/activity/AddShortcutActivity.java
@@ -2,11 +2,22 @@ package com.benny.openlauncher.activity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent; 
 import android.content.pm.LauncherApps;
+import android.content.pm.ShortcutInfo; 
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
-public class AddItemActivity extends Activity {
+import com.benny.openlauncher.AppObject;
+import com.benny.openlauncher.R;
+import com.benny.openlauncher.model.Item;
+import com.benny.openlauncher.util.Definitions;
+import com.benny.openlauncher.util.Tool;
+
+public class AddShortcutActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +32,20 @@ public class AddItemActivity extends Activity {
 
             if (request.getRequestType() == LauncherApps.PinItemRequest.REQUEST_TYPE_SHORTCUT) {
                 
-                ShortcutInfoCompat info = new ShortcutInfoCompat(request.getShortcutInfo());
+                ShortcutInfo info = request.getShortcutInfo();
                 String shortcutLabel = info.getShortLabel().toString();
-                Intent shortcutIntent = info.makeIntent();
-                Drawable shortcutIcon = null;
+                Intent shortcutIntent = new Intent(Intent.ACTION_MAIN)
+                    .setComponent(info.getActivity())
+                    .setPackage(info.getPackage())
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED)
+                    .putExtra("shortcut_id", info.getId());
 
-                App app = Setup.appLoader().createApp(shortcutIntent);
-                Item item;
-                if (app != null) {
-                    item = Item.newAppItem(app);
-                } else {
-                    item = Item.newShortcutItem(shortcutIntent, shortcutIcon, shortcutLabel);
-                }
+                Context context = AppObject.get().getApplicationContext();
+                Drawable shortcutIcon = launcherApps.getShortcutIconDrawable(info, context.getResources().getDisplayMetrics().densityDpi);
+
+                Item item = Item.newShortcutItem(shortcutIntent, shortcutIcon, shortcutLabel);
                 Point preferredPos = HomeActivity.Companion.getLauncher().getDesktop().getPages().get(HomeActivity.Companion.getLauncher().getDesktop().getCurrentItem()).findFreeSpace();
+                
                 if (preferredPos == null) {
                     Tool.toast(HomeActivity.Companion.getLauncher(), R.string.toast_not_enough_space);
                 } else {
